cbff22b485462b8e2db531dadaa82aa16c7e2dc2
Rebecca Song <rebecca.song@outlook.com>
2018-08-06 23:05:13 -0400

    Add site content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Body greeting="hi there!" />
+    <Nav />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Body,
+    Nav,
+    Footer
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f7f7f7;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Consolas, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 1.8rem;
   text-align: center;
+  color: #363d44;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media (max-width: 1300px) {
+  #app {
+    font-size: 1.5rem;
+  }
+}
+
+@media (max-width: 1100px) {
+  #app {
+    font-size: 1.2rem;
+  }
+}
+
+@media (max-width: 480px) {
+  #app {
+    font-size: 1rem;
+  }
+}
+
+@media (max-width: 320px) {
+  #app {
+    font-size: 0.7rem;
+  }
+}
+
+strong {
+  color: #50b608;
+}
+
+a {
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.vue-typer .custom.char {
+  color: #ff7300;
 }
 </style>
