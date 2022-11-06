adc072df018d7f1ca1a6a505536d7a36d1417b3d
Rebecca Song <rebecca.song@outlook.com>
2022-11-06 15:43:53 -0500

    Update to new Vue 3 base

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,47 @@
-<template>
-  <div id="app">
-    <Body greeting="hi there!" />
-    <Nav />
-    <Footer />
-  </div>
-</template>
-
-<script>
-import Nav from "./components/Nav";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-
-export default {
-  name: "app",
-  components: {
-    Body,
-    Nav,
-    Footer
-  }
-};
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
 }
 
-body {
-  background-color: #f7f7f7;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
-#app {
-  font-family: Consolas, monospace;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-size: 1.8rem;
-  text-align: center;
-  color: #363d44;
-  margin: 0 auto;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-@media (max-width: 1300px) {
-  #app {
-    font-size: 1.5rem;
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-}
 
-@media (max-width: 1100px) {
-  #app {
-    font-size: 1.2rem;
+  .logo {
+    margin: 0 2rem 0 0;
   }
-}
 
-@media (max-width: 480px) {
-  #app {
-    font-size: 1rem;
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
   }
-}
-
-@media (max-width: 320px) {
-  #app {
-    font-size: 0.7rem;
-  }
-}
-
-strong {
-  color: #50b608;
-}
-
-a {
-  color: #363d44;
-}
-
-.vue-typer .custom.char {
-  color: #ff7300;
 }
 </style>
